7c7a24cab1ea92d9af202ba5fcd59fec3822d6fb
prayogayogi <yogikpiter@gmail.com>
2023-01-02 23:11:21 +0700

    add model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    /**
+     * fillable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'image', 'title', 'slug', 'category_id', 'content', 'weight', 'price', 'discount'
+    ];
+
+    /**
+     * image
+     *
+     * @return Attribute
+     */
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('/storage/products/' . $value),
+        );
+    }
+}
